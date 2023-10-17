c4d0b772616b3aa9dd4fb0f8e55767c7bd277aee
Alex Bennett <ajbenn90@gmail.com>
2023-10-17 09:08:56 -0600

    Init config

---
+++ 1361/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public bool ValidateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild)
+    {
+        return false;
+    }
+
+    public static void Main()
+    {
+
+    }
+}
