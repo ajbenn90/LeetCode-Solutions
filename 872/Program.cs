e6ddab50fc162dfaa2219296afa46fad5d4d6ed3
Alex Bennett <ajbenn90@gmail.com>
2024-01-09 09:20:19 -0700

    Added solutions for 872 and 938

---
+++ 872/Program.cs
@@ -0,0 +1,48 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool LeafSimilar(TreeNode root1, TreeNode root2)
+    {
+        return GetLeafSequence(root1).SequenceEqual(GetLeafSequence(root2));
+    }
+
+    private List<int> GetLeafSequence(TreeNode root)
+    {
+        List<int> leafSeq = [];
+        Stack<TreeNode> stack = new();
+        stack.Push(root);
+        while (stack.Count > 0)
+        {
+            TreeNode curr = stack.Pop();
+            if (curr.left == null && curr.right == null)
+                leafSeq.Add(curr.val);
+            else
+            {
+                if (curr.right != null)
+                    stack.Push(curr.right);
+                if (curr.left != null)
+                    stack.Push(curr.left);
+            }
+        }
+        return leafSeq;
+    }
+}
+
+public class TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+{
+    public int val = val;
+    public TreeNode left = left;
+    public TreeNode right = right;
+}
---
+++ 938/Program.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int RangeSumBST(TreeNode root, int low, int high) {
+        int sum = 0;
+        Queue<TreeNode> q = new();
+        q.Enqueue(root);
+        while (q.Count > 0)
+        {
+            TreeNode curr = q.Dequeue();
+            if (low <= curr.val && curr.val <= high)
+                sum += curr.val;
+            // if curr.val <= low, then left.val < high since this is a BST and values are unique
+            if (curr.val > low && curr.left != null)
+                q.Enqueue(curr.left);
+            // if curr.val >= high, then right.val > high since this is a BST and values are unique
+            if (curr.val < high && curr.right != null)
+                q.Enqueue(curr.right);
+        }
+        return sum;
+    }
+}
+
+public class TreeNode (int val = 0, TreeNode left = null, TreeNode right = null)
+{
+    public int val = val;
+    public TreeNode left = left;
+    public TreeNode right = right;
+}
