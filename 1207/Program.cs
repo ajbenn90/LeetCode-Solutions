10dd57b044dd127297c8432005bb96b3796b2ebd
Alex Bennett <ajbenn90@gmail.com>
2024-01-17 08:05:07 -0700

    Added 1207 solution

---
+++ 1207/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        Dictionary<int, int> counts = [];
+        foreach (int x in arr)
+        {
+            if (!counts.ContainsKey(x))
+                counts[x] = 0;
+            counts[x]++;
+        }
+        HashSet<int> countsUsed = [];
+        foreach (int x in counts.Keys)
+        {
+            if (countsUsed.Contains(counts[x]))
+                return false;
+            countsUsed.Add(counts[x]);
+        }
+        return true;
+    }
+}
