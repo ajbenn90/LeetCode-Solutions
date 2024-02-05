8ae461697d6a183ea4e74222e8f044192c7e10c1
Alex Bennett <ajbenn90@gmail.com>
2024-02-05 10:08:59 -0700

    Added 387 solution

---
+++ 387/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        int[] counts = new int[26];
+        foreach (char c in s)
+            counts[CharToIdx(c)]++;
+        for (int i = 0; i < s.Length; i++)
+            if (counts[CharToIdx(s[i])] == 1)
+                return i;
+        return -1;
+    }
+
+    private static int CharToIdx(char c)
+    {
+        return (int)c - (int)'a';
+    }
+}
